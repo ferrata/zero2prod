40d6bb47df224ae37d22951a3e06d14cc20e3d61
Sasha Goloshchapov <31716295+ferrata@users.noreply.github.com>
2024-08-03 11:37:56 -0500

    feat: chapter 7 - add status column (3) (#13)

---
+++ migrations/20240803162942_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,9 @@
+-- `sqlx` does not open transactions automatically
+BEGIN;
+    -- Backfill `status` for historical entries
+    UPDATE subscriptions
+        SET status = 'confirmed'
+        WHERE status IS NULL;
+    -- Make `status` mandatory
+    ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+COMMIT;
